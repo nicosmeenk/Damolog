0fe45bbbc748c9f7fb16ae4e53551664188cc192
lfeuer <lodric-bardric@web.de>
2012-01-31 19:01:08 +0100

    ....

---
+++ src/test/java/de/hwr/damolog/gui/MainWindowTest.java
@@ -0,0 +1,14 @@
+package de.hwr.damolog.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainWindowTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
